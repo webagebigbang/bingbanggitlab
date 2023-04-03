b5dab2b4626e935a398f39b45b68968cad5d1764
webage003 <127336395+webage003@users.noreply.github.com>
2023-04-03 13:37:49 -0400

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,77 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+# Attach the internet gateway to the VPC
+resource "aws_vpc_attachment" "example" {
+  vpc_id             = aws_vpc.example.id
+  internet_gateway_id = aws_internet_gateway.example.id
+}
+
+# Create a public subnet
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+# Create a security group for the EC2 instance
+resource "aws_security_group" "instance" {
+  name_prefix = "example-instance-"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create the EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = "my-key"
+  subnet_id     = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+}
